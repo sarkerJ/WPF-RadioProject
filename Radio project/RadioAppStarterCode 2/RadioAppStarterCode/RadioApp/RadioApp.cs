760d814b9d235e13e58218f92091d465ad31ada5
Sarker Jawad Islam <isarkerj@gmail.com>
2020-11-14 12:39:44 +0000

    Added Radio project in repo alone

---
+++ Radio project/RadioAppStarterCode 2/RadioAppStarterCode/RadioApp/RadioApp.cs
@@ -0,0 +1,149 @@
+﻿using Microsoft.Win32.SafeHandles;
+using System;
+using System.IO;
+using System.Windows.Media;
+
+
+namespace RadioApp
+{
+    public class Radio
+    {
+        private int _channel = 1;
+        private bool _on = false;
+        private bool _isPlaying = false;
+        MediaPlayer test = new MediaPlayer();
+       // Random rand = new Random();
+
+        public bool Playing { get { return _isPlaying; }  private set { _isPlaying =value; } }
+
+        public int Channel { 
+            get { return _channel; } 
+            set { if(_on) if(value <=4 && value >=1)_channel = value; } 
+        }
+       
+        //Controls
+        public string Play()
+        { 
+            if (_on)
+            {
+                string playNow = $"Playing {Channels(_channel)}";
+                test.Play();
+                Playing = true;
+                return playNow;
+            }
+            else return "Radio is off";
+        }
+        //off
+        public void TurnOff()
+        {
+            _channel = 1;
+            Playing = false;
+            _on = false;
+            test.Stop();
+        }
+        //on
+        public void TurnOn() {_on = true;}
+
+        //pause
+        public void Pause()  { test.Pause(); }
+
+        //Continue
+        public void Continue()  {test.Play();}
+
+       // Volume Up
+        public double VolUp()
+        {
+            test.Volume += 0.05;
+            return Math.Round(test.Volume * 100, 0);
+        }
+
+
+        //Volume Down
+        public double VolDown()
+        {
+            test.Volume -= 0.05;
+            return Math.Round(test.Volume * 100, 0);
+        }
+
+        //Channels
+        public string Channels(int channel)
+        {
+            string text = "";
+            switch (channel)
+            {
+                case 1:
+                    test.Open(new Uri("http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio1_mf_p"));
+                    text = "Radio 1";
+                    break;
+                case 2:
+                    test.Open(new Uri("http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio2_mf_p"));
+                    text = "Radio 2";
+                    break;
+                case 3:
+                    test.Open(new Uri("http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio3_mf_p"));
+                    text = "Radio 3";
+                    break;
+                case 4:
+                    test.Open(new Uri("http://bbcmedia.ic.llnwd.net/stream/bbcmedia_radio4fm_mf_p"));
+                    text = "Radio 4";
+                    break;
+            }
+            return text;
+        }
+
+        //PlayList
+        string[] audioPath = Directory.GetFiles(@"C:\Users\saadi\OneDrive\Desktop\P files\Songs\", "*.mp3");
+        int song = 0;
+        public string PlayList()
+        {
+            if (_on)
+            {
+                test.MediaEnded += OnMediaEnded;
+                test.Open(new Uri(audioPath[song]));
+                test.Play();
+                return ($"Playing playlist song : {song + 1}");
+            }
+            else return "Radio is off";
+        }
+        //automatically plays the next song
+        private void OnMediaEnded(object sender, EventArgs e)
+        {
+            while (song < audioPath.Length - 1) //after it reaches it plays the last song it keeps playing that same song
+            {
+                song++;
+            }
+            test.Open(new Uri(audioPath[song]));
+            test.Play();
+        }
+
+        //Playlist controls
+        public string NextSong()
+        {
+            if (_on)
+            {
+                song += 1;
+                if (song == audioPath.Length) song = 0;
+                test.Open(new Uri(audioPath[song]));
+                test.Play();
+                return ($"Playing playlist song : {song + 1}");
+            }
+            else return "Radio is off";
+        }
+
+        public string PreviousSong()
+        {
+            if (_on)
+            {
+                song -= 1;
+                if (song < 0) song = audioPath.Length - 1;
+                test.Open(new Uri(audioPath[song]));
+                test.Play();
+                return ($"Playing playlist song : {song + 1}");
+            }
+            else return "Radio is off";
+        }
+
+    }
+    // implement a class Radio that corresponds to the Class diagram 
+    //   and specification in the Radio_Mini_Project document
+}
---
+++ Radio project/RadioAppStarterCode 2/RadioAppStarterCode/RadioGUI/MainWindow.xaml.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using RadioApp;
+using System.Threading;
+
+
+namespace RadioGUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Radio myRadio = new Radio();
+        BrushConverter bc = new BrushConverter();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        private void RadioControl_Click(object sender, RoutedEventArgs e)
+        {
+            Button controlB = (Button)sender;
+            switch(controlB.Content)
+            {
+                case "On":
+                    myRadio.TurnOn();
+                    DisplayBox.Text = "On";
+                    DisplayBox.Background = Brushes.Azure;
+                    break;
+                case "Off":
+                    myRadio.TurnOff();
+                    DisplayBox.Background = (Brush)bc.ConvertFrom("#FFF7F6F6");
+                    DisplayBox.Text = "";
+                    break;
+                case "Play":
+                    DisplayBox.Text = myRadio.Play();
+                    break;
+                case "Pause":
+                    myRadio.Pause();
+                    BPause.Content = "Continue";
+                    break;
+                case "Continue":
+                    myRadio.Continue();
+                    BPause.Content = "Pause";
+                    break;
+                case "Volume Up":
+                    DisplayBox.Text = $"Volume: {myRadio.VolUp()}";
+                    break;
+                case "Volume Down":
+                    DisplayBox.Text = $"Volume: {myRadio.VolDown()}"; 
+                    break;
+            }
+        }
+        private void Channel_Click(object sender, RoutedEventArgs e)
+        {  
+            Button b = (Button)sender;
+            switch(b.Content) 
+            {
+                case "1":
+                    myRadio.Channel = 1;
+                    break;
+                case "2":
+                    myRadio.Channel = 2;
+                    break;
+                case "3":
+                    myRadio.Channel = 3;
+                    break;
+                case "4":
+                    myRadio.Channel = 4;
+                    break;
+            }
+            if (myRadio.Playing) DisplayBox.Text = myRadio.Play();
+
+        }
+        private void Playlist_Click(object sender, RoutedEventArgs e)
+        {
+            Button mine = (Button)sender;
+
+            switch (mine.Content)
+            {
+                case "PlayList":
+                    DisplayBox.Text = myRadio.PlayList();
+                    break;
+                case "Next":
+                    DisplayBox.Text = myRadio.NextSong();
+                    break;
+                case "Previous":
+                    DisplayBox.Text = myRadio.PreviousSong();
+                    break;
+            }
+        }
+    }
+}
